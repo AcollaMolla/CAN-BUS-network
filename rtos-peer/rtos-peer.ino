bb49d4d794072f46e87c4fed24a4ae7ad466252e
Anton <antengman@gmail.com>
2021-02-09 19:55:48 +0100

    Create instansiation of CAN in rtos-peer.ino

--- rtos-peer/rtos-peer.ino
+++ rtos-peer/rtos-peer.ino
@@ -1,14 +1,24 @@
 #include <Arduino_FreeRTOS.h>
+#include "mcp2515_can.h"
 
-void Task1(void *pvParameters);
-void Task2(void *pvParameters);
+void sendCAN(void *pvParameters);
+void receiveCAN(void *pvParameters);
+
+const int SPI_CS_PIN = 10;
+mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
 
 void setup(){
-  Serial.begin(9600);
-  pinMode(10, OUTPUT);
-  digitalWrite(10, HIGH);
-  xTaskCreate(Task1, "task1", 128, NULL, 3, NULL);
-  xTaskCreate(Task2, "task2", 128, NULL, 2, NULL);
+  SERIAL_PORT_MONITOR.begin(115200);
+  while(!Serial){};
+
+  while (CAN_OK != CAN.begin(CAN_125KBPS)) {             // init can bus : baudrate = 500k
+    SERIAL_PORT_MONITOR.println("CAN init fail, retry...");
+    delay(100);
+  }
+  SERIAL_PORT_MONITOR.println("CAN init ok!");
+
+  xTaskCreate(sendCAN, "sendCAN", 128, NULL, 3, NULL);
+  xTaskCreate(receiveCAN, "receiveCAN", 128, NULL, 2, NULL);
   vTaskStartScheduler();
 }
 
@@ -16,24 +26,14 @@ void loop(){
   
 }
 
-void Task1(void *pvParameters){
-  pinMode(8, OUTPUT);
+void sendCAN(void *pvParameters){
   while(1){
-    Serial.println("Task1");
-    digitalWrite(8, HIGH);
-    vTaskDelay(200/portTICK_PERIOD_MS);
-    digitalWrite(8, LOW);
     vTaskDelay(200/portTICK_PERIOD_MS);
   }
 }
 
-void Task2(void *pvParameters){
-  pinMode(7, OUTPUT);
+void receiveCAN(void *pvParameters){
   while(1){
-    Serial.println("Task2");
-    digitalWrite(7, HIGH);
-    vTaskDelay(100/portTICK_PERIOD_MS);
-    digitalWrite(7, LOW);
     vTaskDelay(100/portTICK_PERIOD_MS);
   }
 }
