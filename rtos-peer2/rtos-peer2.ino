93e7da3d89d19ed38b8b9b4d53e04caff8573fd3
Anton <antengman@gmail.com>
2021-02-09 20:09:25 +0100

    Clean up in RTOS code in rtos-peer2.ino

--- rtos-peer2/rtos-peer2.ino
+++ rtos-peer2/rtos-peer2.ino
@@ -1,14 +1,11 @@
 #include <Arduino_FreeRTOS.h>
 
-void Task1(void *pvParameters);
-void Task2(void *pvParameters);
+void sendCAN(void *pvParameters);
+void receiveCAN(void *pvParameters);
 
 void setup(){
-  Serial.begin(9600);
-  pinMode(10, OUTPUT);
-  digitalWrite(10, HIGH);
-  xTaskCreate(Task1, "task1", 128, NULL, 3, NULL);
-  xTaskCreate(Task2, "task2", 128, NULL, 2, NULL);
+  xTaskCreate(sendCAN, "sendCAN", 128, NULL, 3, NULL);
+  xTaskCreate(receiveCAN, "receiveCAN", 128, NULL, 2, NULL);
   vTaskStartScheduler();
 }
 
@@ -16,24 +13,14 @@ void loop(){
   
 }
 
-void Task1(void *pvParameters){
-  pinMode(8, OUTPUT);
+void sendCAN(void *pvParameters){
   while(1){
-    Serial.println("Task1");
-    digitalWrite(8, HIGH);
-    vTaskDelay(200/portTICK_PERIOD_MS);
-    digitalWrite(8, LOW);
-    vTaskDelay(200/portTICK_PERIOD_MS);
+    vTaskDelay(1000/portTICK_PERIOD_MS);
   }
 }
 
-void Task2(void *pvParameters){
-  pinMode(7, OUTPUT);
+void receiveCAN(void *pvParameters){
   while(1){
-    Serial.println("Task2");
-    digitalWrite(7, HIGH);
-    vTaskDelay(100/portTICK_PERIOD_MS);
-    digitalWrite(7, LOW);
     vTaskDelay(100/portTICK_PERIOD_MS);
   }
 }
