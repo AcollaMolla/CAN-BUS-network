02b39478664318b9ccbbec1656fc7221eedc9cd6
Anton <antengman@gmail.com>
2021-02-09 20:15:05 +0100

    Added RTOS threads for sending/receiving on CAN BUS

--- rtos-peer2/rtos-peer2.ino
+++ rtos-peer2/rtos-peer2.ino
@@ -1,9 +1,22 @@
 #include <Arduino_FreeRTOS.h>
+#include "mcp2515_can.h"
 
 void sendCAN(void *pvParameters);
 void receiveCAN(void *pvParameters);
 
+const int SPI_CS_PIN = 10;
+mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
+unsigned char response[2] = {0x3e, 0x1};
+
 void setup(){
+  SERIAL_PORT_MONITOR.begin(115200);
+  while(!Serial){};
+  while (CAN_OK != CAN.begin(CAN_125KBPS)) {             // init can bus : baudrate = 500k
+    SERIAL_PORT_MONITOR.println("CAN init fail, retry...");
+    delay(100);
+  }
+  
+  SERIAL_PORT_MONITOR.println("CAN init ok!");
   xTaskCreate(sendCAN, "sendCAN", 128, NULL, 3, NULL);
   xTaskCreate(receiveCAN, "receiveCAN", 128, NULL, 2, NULL);
   vTaskStartScheduler();
@@ -15,12 +28,23 @@ void loop(){
 
 void sendCAN(void *pvParameters){
   while(1){
+    CAN.sendMsgBuf(0x3fe, 0, 2, response);
+    SERIAL_PORT_MONITOR.println("Replied!");
     vTaskDelay(1000/portTICK_PERIOD_MS);
   }
 }
 
 void receiveCAN(void *pvParameters){
+  unsigned char len = 0;
+  unsigned char buf[8];
   while(1){
+    if (CAN_MSGAVAIL == CAN.checkReceive()) {         // check if data coming
+      CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+      unsigned long canId = CAN.getCanId();
+      if(buf[0] == 0x40){
+        SERIAL_PORT_MONITOR.println("Someone called me!");
+      }
+    }
     vTaskDelay(100/portTICK_PERIOD_MS);
   }
 }
